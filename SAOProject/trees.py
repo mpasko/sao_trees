2eff6ce987396191e5969d777d87d6e495988b49
xmichaelx <michal.drzal@gmail.com>
2014-05-04 17:47:41 +0200

    Rewritten code to use NetworkX for graph manipulations.

--- SAOProject/trees.py
+++ SAOProject/trees.py
@@ -1,104 +1,94 @@
-import random
+from random import choice
+from re import split
 import numpy as np
-
-def edge_number(e):
-    """Tricky way to convert edge into serial number"""
-    """Enumerating as 21,31,32,41,42,43,51,52,53,54,..."""
-    edge = e
-    #assume first goes biger number
-    if e[0]<e[1]:
-        edge = [e[1],e[0]]
-    x = e[0]-2
-    #serial number of first occurence of edge with e[0] (like an arithmetical string)
-    first = x * (x + 1) / 2
-    #difference grows up since first occurence of e[0]
-    difference = e[0]-e[1]
-    
-    #here we go
-    return first + difference - 1
+import networkx as nx
 
 class Graph:
-    """Graph data encapsulated"""
-    size=0
-    pairWeights=[]
+    """Graph data loaded from file"""       
+    def __init__(self,filename):
+        with open(filename) as f:
+            # loading from file
+            order = int(f.readline().replace("param n :=", "").replace(";", ""))
+            size = int(f.readline().replace("param m :=", "").replace(";", ""))
+            f.readline()
+            edgesLine = [x for x in f.readline().replace("param c := ", "").replace(" ;", "").split("\t") if "\n" not in x]
+            edges = [[int(y) for y in split('\[|\]|,',x) if y != ''] for x in edgesLine]
+            f.readline()
+            edgePairsLine = [x for x in f.readline().replace(" ;", "").split("\t")  if "\n" not in x]
+            edgePairs = [[int(y) for y in split('\[|\]|,',x) if y != ''] for x in edgePairsLine]
 
-    def len(self):
-        return self.size
+            # converting to representation recognizable by networkX
+            edgeList = [(edge[0]-1, edge[1]-1, { 'weight' : edge[2] } ) for edge in edges]
+            # mapping between [v1,v2] and edge number used in q
+            v2e = {(x[0],x[1]) : edgeList.index(x) for x in edgeList } 
+            
+            edgePairList = [(v2e[(edge[0]-1, edge[1]-1)], v2e[(edge[2]-1, edge[3]-1)], { 'weight' : edge[4] } ) for edge in edgePairs]
         
-    def __init__(self,vertices):
-        self.vertices = vertices
-        edges = int(vertices*(vertices-1)/2)
-        graph = np.random.random_sample((edges, edges))
-        #Removing diagonal entries, making matrix simmetric
-        for x in range(0, total):
-            for y in range(0, total):
-                if x-y>0:
-                    sample[x][y] = sample[y][x]
-                elif x==y:
-                    sample[x][y] = 0.0
-        self.pairWeights=sample
-    def debug(self):
-        print(self.pairWeights)
-        
-    def getValue(self,e1,e2):
-        return self.pairWeights[edge_number(e1)][edge_number(e2)]
+            self.c = nx.Graph(edgeList)  # adjacency matrix
+            self.q = nx.Graph(edgePairList) # matrix of costs between pairs of edges
+            self.v2e = v2e
 
-class Tree:
+
+class SpanningTree:
     """Tree data along with operations to manipulate it"""
-    data=[]
-    size=0
-    weight=0.0
-    def randomize(self):
-        self.data=[ [x,random.randint(x+1,self.size-1)] for x in range(0, self.size-1)]
-    
-    def recalc_target(self):
-        self.weight=0.0
-        for a in self.data:
-            for b in self.data:
-                self.weight = self.weight + self.graph.getValue(a,b)
-    
     def __init__(self,graph):
-        self.size=graph.len()
-        self.graph=graph
-        self.randomize()
-        self.recalc_target()
-        
+        self.graph = graph
+        self.tree = self.randomSpanningTree()
+
+    """ Generating random spanning tree using DFS search. Connecting each vertex to its predecessor generates spanning tree. """
+    def randomSpanningTree(self):
+        graph = self.graph.c
+        source = choice(graph.nodes()) # random start element
+        spanning_tree = nx.Graph()
+        # random spanning tree from predecessors
+        spanning_tree.add_edges_from([(u,v) for u,v in nx.dfs_predecessors(graph, source).items()])
+        return spanning_tree
+    
+    """ Cost of solution. The lower the better. """
+    def getCost(self):
+        edgeCosts = self.graph.c
+        pairCosts = self.graph.q
+        tree = self.tree
+
+        s = sum([edgeCosts.get_edge_data(uv[0],uv[1])["weight"] for uv in tree.edges()])
+        edgePairs = [(self.graph.v2e[x], self.graph.v2e[y]) for x in tree.edges() for y in tree.edges()]
+        s += sum([pairCosts.get_edge_data(e[0],e[1])["weight"] for e in edgePairs if e[0] != e[1]]) 
+
+        return s
+    
+    """ Makes random change to the state """
     def randomChange(self):
-        selection=random.randint(0,self.size-2)
-        #edge chosen to remove
-        removed=self.data[selection]
-        del self.data[selection]
-        #produce merging sets (we can still travel to vertexes "navigability[x]" from vertex x)
-        navigability=[ set([x]) for x in range(0, self.size)]
+        # edge chosen to remove
+        tree = self.tree 
+        graph= self.graph.c
+        v1 = choice(tree.nodes())
+        v2 = choice(tree.neighbors(v1))
         #removal of an edge splits tree into 2 subtrees
-        subset1=navigability[removed[0]]
-        subset2=navigability[removed[1]]
-        def merge(s_a, s_b):
-            s_a.update(s_b)
-            s_b.update(s_a)
-        for edge in self.data:
-            merge(navigability[edge[0]],navigability[edge[1]])
-        #last thing is to choose 2 vertexes from both sets to join with new edge
-        list1=[x for x in subset1]
-        list2=[y for y in subset2]
-        a=random.randint(0,len(list1)-1)
-        b=random.randint(0,len(list2)-1)
-        inserted=[list1[a],list2[b]]
-        self.data.append(inserted)
-        self.recalc_target()
-        
-    def debug(self):
-        print(self.data, "target:",self.weight)
-        
-    def targetFunction(self):
-        return self.weight
+        tree.remove_edge(v1,v2)
+
+        # we obtain nodes accesible from each vertex
+        n1 = nx.dfs_tree(tree, v1).nodes()
+        n2 = nx.dfs_tree(tree, v2).nodes()
+
+        # we list possible candidates for new connection
+        possible_connections = [(u,v) for u in n1 for v in n2 if graph.has_edge(u, v)]
+        possible_connections.remove((v1,v2)) # remove previous connection
+
+        if possible_connections == []:
+            tree.add_edge(v1, v2) # no changes
+            print("no change")
+        else:
+            edge = choice(possible_connections)
+            tree.add_edge(edge[0], edge[1]) # new node
+            print(str((v1,v2)) + " -> " + str(edge))
         
         
 if __name__ == '__main__':
-    g=Graph(5)
-    print(g.len())
-    t=Tree(g)
-    t.debug()
-    for x in range(0,40):
+    g = Graph("benchmark/n010d033C010c001Q010q001s-3i1.txt")
+    t = SpanningTree(g)
+    print(t.getCost())
+    for i in range(30):
         t.randomChange()
-        t.debug()
+        print(t.getCost())
+
+    
